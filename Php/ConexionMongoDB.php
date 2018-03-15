dcf68685bd480c4c3f99979c2950bb6e1603f0f2
Eric Avila <euab@MacBook-Pro-de-Eric.local>
2018-03-14 23:48:20 -0600

    Se actualiza un nuevo metodo para la conexion y se modifica el mostrarTodo

--- Php/ConexionMongoDB.php
+++ Php/ConexionMongoDB.php
@@ -6,22 +6,21 @@ require 'vendor/autoload.php';
 * Conector MongoDB PHP
 */
 
-class _MongoDB 
-{
-	
-
-	function Conector()
-	{
-		try {
-		$db = "db_euab";
-		$collection = "Heroes";
-		$uri = "mongodb://euab:admincirex@ds253468.mlab.com:53468/".$db;
-		$collection = (new MongoDB\Client($uri))->$db->$collection;
-
-		echo "Se establecio la conexion a la Base de Datos ".$db." y se utiliza la Coleccion ".$collection;
-
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-	}
-}
+ class _MongoDB 
+ {
+  function C_DB()
+ 	{
+ 		 try {
+ 		 $db = "db_euab";
+ 		 $collection = "Heroes";
+ 		 $uri = "mongodb://euab:admincirex@ds253468.mlab.com:53468/".$db;
+ 		 return $collection = (new MongoDB\Client($uri))->$db->$collection;
+ 			//var_dump("Se establecio la conexion a la Base de Datos ".$db." y se utiliza la Coleccion ".$collection);
+ 		 } catch (MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
+ 		 	error_log($e);
+ 		 }
+ 	}
+ }
+ $obj = new  _MongoDB();
+ $obj->C_DB();
+?>
--- Php/mostrarTodo.php
+++ Php/mostrarTodo.php
@@ -1,18 +1,25 @@
 <?php 
 
 require 'vendor/autoload.php';
+require 'ConexionMongoDB.php';
+
+
 
 function mostrarTodo(){
 
+$obj = new  _MongoDB();
+$collection = $obj->C_DB();
 
 try {
 
-	$uri = "mongodb://euab:admincirex@ds253468.mlab.com:53468/db_euab";
-	$collection = (new MongoDB\Client($uri))->db_euab->Heroes;
+	// $uri = "mongodb://euab:admincirex@ds253468.mlab.com:53468/db_euab";
+	// $collection = (new MongoDB\Client($uri))->db_euab->Heroes;
+	
+
 	$resultArray = $collection->find([]); 
 
 } catch (MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
-	echo $e;
+	error_log ($e);
 }
 
 
